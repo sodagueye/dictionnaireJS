e3caaa61628f38a4ea942d39282c4d06b3711ee6
soda gueye <gueyesoda56@gmail.com>
2023-12-12 16:52:51 +0100

    modification

--- dictEng.js
+++ dictEng.js
@@ -1,25 +1,48 @@
 const wrapper = document.querySelector(".wrapper");
 const searchInput = document.querySelector("input");
 const infoText = document.querySelector(".info-text");
-// data functon
+const synonyms = document.querySelector(".synonyms .list");
+const volume =document.querySelector(" audio")
+let audio;
+// Fonction de données 
 function data(result ,word){
-    console.log(result)
+    //  console.log(result)
+    if(result.title){
+        // message erreur
+        infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+    }else{
+        console.log(result);
+        wrapper.classList.add("active");
+        let definitions=result[0].meanings[0].definitions[0];
+        //passons les données de réponse particulières à un élément HTML particulier
+         document.querySelector(".word p ").innerText=`Word title :${result[0].word}`;
+
+         document.querySelector(".meaning span").innerText= definitions.definition;
+    
+        // creating new audio obj and passing audio src
+        audio =new Audio("https:"+result[0].phonetics[0].audio) ;
+        
+
+    }
 }
 // flech api function
 function fetchApi(word){
-    // infoText.style.color ="#000"
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
-    let url=`https://api.dictionaryapi.dev/api/v2/entries/en/<word>`;
-    // fetching api response and returning it parsing into js obj and in nather then 
-    // method calling data function with paasing api response and searched word as an argument
-  fetch(url).then(res => res.json()).then(result => data(result ,word));
+    let url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    
+   fetch(url)
+  .then(res => res.json())
+  .then(result => data(result ,word));
 
 
 }
-searchInput.addEventListener("keyup",e => {
-    if(e.key=="Enter"&& e.target.value){
+searchInput.addEventListener(`keyup`,e => {
+    if(e.key== "Enter" && e.target.value){
         fetchApi(e.target.value)
 }
 //  searchInput.value="";
+});
+volume.addEventListener('click',()=>{
+    audio.play();
 });
 
